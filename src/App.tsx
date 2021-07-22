079f9d4a50dcdd826849d416efc6ee41c55d6cd0
”Patron” <victor_sinitca@mail.ru>
2021-07-22 17:30:46 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TableComponent} from "./Component/TableComponent";
 
 function App() {
-  return (
-    <div className="App">
-2
-    </div>
-  );
+    const dataUrl = {
+        headerContent: [
+            "Column1",
+            "Column1",
+            "Column1",
+            "Column1",
+            "Column1",
+            "Column1",
+        ],
+        tableContent:[
+
+        ]
+
+    }
+
+    return (
+        <div className="App">
+            <TableComponent/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Component/TableComponent.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from "react";
+
+
+export const TableComponent:FC<{}>=()=>{
+
+    return<div>
+        rrrr
+    </div>
+}
